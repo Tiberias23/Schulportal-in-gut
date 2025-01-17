fd5a63a00649d295e9b5ea7e009068a020df5f01
Tiberias23 <tiberias.peters@gmail.com>
2025-01-17 10:48:00 +0100

    Teilweise K

--- app/database.py
+++ app/database.py
@@ -2,4 +2,9 @@ import sqlite3
 
 db = sqlite3.connect('app.db')
 
-#db.cursor().execute('CREATE TABLE IF NOT EXISTS users ()')
+# Create a table for users if it doesn't exist
+db.cursor().execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)').close()
+# Hier kann man einen SQLite Befehl ausführen.
+db.cursor().execute("SELECT * FROM users").close()
+# Commit the changes
+db.commit()
