a8c6b7eea84468084f66cb92bfddddf5d7e6bd9d
Tiberias23 <tiberias.peters@gmail.com>
2025-01-17 10:52:10 +0100

    Teilweise Kommentare eingefügt

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,12 @@ from .database import db
 
 main = Blueprint('main', __name__)
 
+# Läd die Login Seite (Funktioniert noch nicht)
 @main.route('/api/login', methods=['POST'])
 def api():
     return jsonify({}), 200, { 'Content-Type': 'application/json' }
 
+# Läd die HTML Dateien aus dem templates Ordner
 @main.route('/<path:path>')
 def home(path):
     return send_from_directory('templates', path)
--- run.py
+++ run.py
@@ -2,5 +2,6 @@ from app import create_app
 
 app = create_app()
 
+# Starten der Anwendung
 if __name__ == '__main__':
     app.run(debug=True)  # Debug-Modus aktivieren
