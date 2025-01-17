c3e146e66713e291aeedba3164da2c7acb63e07c
Tiberias23 <tiberias.peters@gmail.com>
2025-01-17 11:37:40 +0100

    Datenban geupdatet

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import sqlite3
 db = sqlite3.connect('app.db')
 
 # Create a table for users if it doesn't exist (Bei zweiteFremdsprache bitte nur 0 oder 1 eingeben (0 = Nein, 1 = Ja))
-db.cursor().execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, Klasse TEXT, zweiteFremdsprache INTEGER, email TEXT, password TEXT)').close()
+db.cursor().execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT,email TEXT, password TEXT)').close()
 # Hier kann man einen SQLite Befehl ausführen.
 db.cursor().execute("").close()
 # Commit the changes
