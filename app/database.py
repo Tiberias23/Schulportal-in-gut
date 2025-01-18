875054275bf18594dedf473513043eb0456bd986
Tiberias23 <tiberias.peters@gmail.com>
2025-01-18 09:28:57 +0100

    push

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 import sqlite3
-import argon2
+from argon2.exceptions import VerifyMismatchError
 db = sqlite3.connect('app.db')
 
 def verify_user(username, password):
@@ -7,11 +7,11 @@ def verify_user(username, password):
         cursor = db.cursor()
         cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
         return cursor.fetchone()
-    except argon2.exceptions.VerifyMismatchError:
+    except VerifyMismatchError:
         return False, "Invalid password"
 # Create a table for users if it doesn't exist (Bei zweiteFremdsprache bitte nur 0 oder 1 eingeben (0 = Nein, 1 = Ja))
 db.cursor().execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT,email TEXT, password TEXT)').close()
 # Hier kann man einen SQLite Befehl ausführen.
-db.cursor().execute("").close()
+db.cursor().execute("SELECT* FROM users").close()
 # Commit the changes
 db.commit()
